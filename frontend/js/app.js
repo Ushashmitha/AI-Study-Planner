f0ccb5f20d801a9ad718f9708f9605fae242c325
Ushashmitha <ushashmitha6@gmail.com>
2026-08-11 23:29:51 +0530

    Create initial dashboard frontend

---
+++ frontend/js/app.js
@@ -0,0 +1,5 @@
+const generatePlanButton = document.getElementById("generatePlanButton");
+
+generatePlanButton.addEventListener("click", () => {
+    alert("Study plan generation will be connected soon!");
+});
